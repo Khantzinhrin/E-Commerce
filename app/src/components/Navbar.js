595cb32761232c1955ea3d2967557fd03e5c2d92
Khant Zin Hein <heinkhantzin0@gmail.com>
2025-10-26 12:29:16 +0630

    Initial Commit

---
+++ app/src/components/Navbar.js
@@ -0,0 +1,13 @@
+export default function NavBar(){
+    return (
+        <nav>
+            <h1>E-Commerce Site</h1>
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
